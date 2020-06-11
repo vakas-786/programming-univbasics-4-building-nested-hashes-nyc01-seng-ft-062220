fd7ad752b99158055f1c0963e90a15c0a2f481dd
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-11 23:51:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,12 @@ def monopoly_with_third_tier
   new_hash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {},
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
       :names => {}
     }
   }
